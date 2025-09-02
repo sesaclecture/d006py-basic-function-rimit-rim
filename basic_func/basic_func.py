442a02fdaf6ed0b38195a7d3d2378b8630d9f107
surim <yeom.surim@gmail.com>
2025-09-02 11:23:27 +0900

    solved basic_func.py

--- basic_func/basic_func.py
+++ basic_func/basic_func.py
@@ -1,26 +1,40 @@
-def add(a, b):
-    pass
+import math
+import numpy as np
 
+def add(a, b):
+    result = a + b
+    return result
 
 def sub(a, b):
-    pass
+    result = a - b
+    return result
 
 
 def mul(a, b):
-    pass
+    result = a * b
+    return result
 
 
 def div(a, b):
-    pass
+    result = a / b
+    return result
 
 
 def power(base, pow):
-    pass
+    result = base ** pow
+    return result
 
 
 def square(base):
-    pass
+    return base * base
 
 
 def greet(이름="낯선자", 나이=20):
-    pass
+    인사 = ""
+    if (나이 >= 40):
+        인사 = "안녕하십니까"
+    elif (나이 >= 20):
+        인사 = "안녕하신가"
+    else:
+        인사 = "안녕"
+    return 인사 + f" {이름}!"
