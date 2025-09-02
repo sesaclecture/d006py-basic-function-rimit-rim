7ee324109c2b64e7f74607e8fde05f84cbbee3e2
surim <yeom.surim@gmail.com>
2025-09-02 11:31:00 +0900

    retry

--- basic_func/basic_func.py
+++ basic_func/basic_func.py
@@ -2,22 +2,18 @@ import math
 import numpy as np
 
 def add(a, b):
-    result = a + b
-    return result
+    return a + b
 
 def sub(a, b):
-    result = a - b
-    return result
+    return a - b
 
 
 def mul(a, b):
-    result = a * b
-    return result
+    return a * b
 
 
 def div(a, b):
-    result = a / b
-    return result
+    return a / b
 
 
 def power(base, pow):
